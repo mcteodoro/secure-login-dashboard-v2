8adc3ea5dfeae88e6820e0a0e6b8c4630c56b7a6
mcteodoro <161388910+mcteodoro@users.noreply.github.com>
2026-05-20 11:39:53 -0300

    Frontend online

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../")));
 
 const express = require("express");
 const cors = require("cors");
