d2621ea68fe983be1c596c45e02a1c796100a46f
mcteodoro <161388910+mcteodoro@users.noreply.github.com>
2026-05-20 12:16:04 -0300

    Fix mongo

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB conectado"))
   .catch((erro) => console.log("Erro ao conectar MongoDB:", erro));
 
